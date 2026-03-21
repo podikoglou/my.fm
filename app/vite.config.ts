93e48199c20fafdffe57da0c15889d6f22b131f5
alex <alex.podikoglou@gmail.com>
2026-03-21 15:27:56 +0200

    style(app): format

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    allowedHosts: ['my.fm']
-  }
+    allowedHosts: ["my.fm"],
+  },
 });
