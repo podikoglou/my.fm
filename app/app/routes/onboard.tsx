e54ded35662b2aa96286bfbd717eafc6fffc63f7
alex <alex.podikoglou@gmail.com>
2026-03-25 02:59:23 +0200

    feat(app): hero for onboarding page

--- app/app/routes/onboard.tsx
+++ app/app/routes/onboard.tsx
@@ -15,6 +15,7 @@ import {
 import { accessTokenAtom } from "~/state/auth";
 import { userAtom } from "~/state/user";
 import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 
 export async function clientLoader() {
   const store = getDefaultStore();
@@ -56,7 +57,12 @@ export default function Onboard() {
   };
 
   return (
-    <>
+    <div className="flex w-96 flex-col gap-6">
+      <div>
+        <h1 className="text-4xl font-extrabold">One more thing...</h1>
+        <p>We need some more information about you to know you better.</p>
+      </div>
+
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-96 flex-col gap-4">
           <FormField
@@ -110,8 +116,10 @@ export default function Onboard() {
               />
             )}
           />
+
+          <Button type="submit">Submit</Button>
         </form>
       </Form>
-    </>
+    </div>
   );
 }
