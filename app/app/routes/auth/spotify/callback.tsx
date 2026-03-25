e40569b6a26eb0fbd27cc17fbe62377161e26aa1
alex <alex.podikoglou@gmail.com>
2026-03-25 12:47:40 +0200

    docs(app): TODo

--- app/app/routes/auth/spotify/callback.tsx
+++ app/app/routes/auth/spotify/callback.tsx
@@ -17,6 +17,7 @@ export async function clientLoader({ request }: Route.ClientLoaderArgs) {
   const client = makeClient(null);
 
   // send authorization code to our backend, expecting back a JWT
+  // TODO: make this a thing in lib/api.ts
   const { accessToken } = await client
     .post("auth/spotify", {
       json: {
