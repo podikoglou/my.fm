dd470001f829946ec2e45d7f6792b7bd91df5bbb
alex <alex.podikoglou@gmail.com>
2026-03-25 02:53:04 +0200

    feat(app): layout

--- app/app/routes.ts
+++ app/app/routes.ts
@@ -1,10 +1,12 @@
 import { type RouteConfig, index, route, prefix, layout } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
+  layout("routes/layout.tsx", [
+    index("routes/home.tsx"),
 
-  ...prefix("app", [layout("routes/app/layout.tsx", [index("routes/app/index.tsx")])]),
+    ...prefix("app", [layout("routes/app/layout.tsx", [index("routes/app/index.tsx")])]),
 
-  route("onboard", "routes/onboard.tsx"),
-  ...prefix("auth", [route("spotify/callback", "routes/auth/spotify/callback.tsx")]),
+    route("onboard", "routes/onboard.tsx"),
+    ...prefix("auth", [route("spotify/callback", "routes/auth/spotify/callback.tsx")]),
+  ]),
 ] satisfies RouteConfig;
