e68eaa68d13f7ed85e8354d3db75d892d1ac73a5
alex <alex.podikoglou@gmail.com>
2026-03-25 02:39:01 +0200

    feat(app): properly set ui 9ui

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="root">
         {children}
         <ScrollRestoration />
         <Scripts />
