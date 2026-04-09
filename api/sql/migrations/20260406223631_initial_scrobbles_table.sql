483f04c1485a1503daeb10c8dac1b2d1c57ed139
alex <alex.podikoglou@gmail.com>
2026-04-09 15:05:59 +0300

    db(app): scrobbles table migration

---
+++ api/sql/migrations/20260406223631_initial_scrobbles_table.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE scrobbles (
+  id          TEXT PRIMARY KEY,
+  user_id     TEXT NOT NULL,
+
+  created_at  TEXT NOT NULL,
+
+  spotify_uri TEXT NOT NULL
+);
+
+-- +goose Down
+DROP TABLE scrobbles;
