9e169ea50a3bc0e27cf696370147ef7644c35e7f
alex <alex.podikoglou@gmail.com>
2026-04-09 15:17:43 +0300

    style(api): format SQL

--- api/sql/migrations/20260406223631_initial_scrobbles_table.sql
+++ api/sql/migrations/20260406223631_initial_scrobbles_table.sql
@@ -1,10 +1,8 @@
 -- +goose Up
-CREATE TABLE scrobbles (
-  id          TEXT PRIMARY KEY,
-  user_id     TEXT NOT NULL,
-
-  created_at  TEXT NOT NULL,
-
+CREATE TABLE scrobbles(
+  id TEXT PRIMARY KEY,
+  user_id TEXT NOT NULL,
+  created_at TEXT NOT NULL,
   spotify_uri TEXT NOT NULL
 );
 
--- api/sql/query.sql
+++ api/sql/query.sql
@@ -1,36 +1,25 @@
 -- name: GetUserById :one
-SELECT * FROM users
-WHERE id = ?
-LIMIT 1;
+SELECT * FROM users WHERE id = ? LIMIT 1;
 
 -- name: GetUserByEmail :one
-SELECT * FROM users
-WHERE email = ?
-LIMIT 1;
+SELECT * FROM users WHERE email = ? LIMIT 1;
 
 -- name: GetUserByUsername :one
-SELECT * FROM users
-WHERE username = ?
-LIMIT 1;
+SELECT * FROM users WHERE username = ? LIMIT 1;
 
 -- name: CreateUser :one
-INSERT INTO users (
-  id, username, name, email, onboarded
-) VALUES (
-  ?, ?, ?, ?, FALSE
-)
+INSERT INTO users(id, username, name, email, onboarded)
+VALUES (?, ?, ?, ?, FALSE)
 RETURNING *;
 
 -- name: UpdateUserSpotifyTokens :exec
 UPDATE users
-set spotify_access_token = ?,
-    spotify_refresh_token = ?,
-    spotify_token_expiration = ?
-WHERE id = ?;
+SET
+  spotify_access_token = ?,
+  spotify_refresh_token = ?,
+  spotify_token_expiration = ?
+WHERE
+  id = ?;
 
 -- name: OnboardUser :exec
-UPDATE users
-set username = ?,
-    name = ?,
-    onboarded = true
-WHERE id = ?;
+UPDATE users SET username = ?, name = ?, onboarded = true WHERE id = ?;
