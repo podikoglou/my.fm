8840e483641eda5de8e516c335f897b5f93cbe41
alex <alex.podikoglou@gmail.com>
2026-04-11 21:31:52 +0300

    feat(api): also parse SPOTIFY_CLIENT_SECRET

--- api/src/env.ts
+++ api/src/env.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   SPOTIFY_CLIENT_ID: z.string(),
+  SPOTIFY_CLIENT_SECRET: z.string(),
   SPOTIFY_REDIRECT_URI: z.string(),
   DATABASE_URL: z.string(),
   SECRET: z.string(),
