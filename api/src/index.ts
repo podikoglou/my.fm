f1f3350d00b2d6f8d23c3d237b1b681e172b3e8f
alex <alex.podikoglou@gmail.com>
2026-04-13 01:32:14 +0300

    feat(api): cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,12 +3,13 @@ import type { JwtVariables } from "hono/jwt";
 import auth from "./routes/auth";
 import user from "./routes/user";
 import type { User } from "./db/schema";
+import { cors } from "hono/cors";
 
 export type Env = {
   Variables: JwtVariables & { getUser: () => Promise<User> };
 };
 
-const app = new Hono<Env>().route("/auth", auth).route("/user", user);
+const app = new Hono<Env>().use("/*", cors()).route("/auth", auth).route("/user", user);
 
 export type AppType = typeof app;
 
