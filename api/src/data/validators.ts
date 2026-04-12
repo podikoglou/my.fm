b7ba90045bf4bf1bd08c95ccb1418d6d6b1f7290
alex <alex.podikoglou@gmail.com>
2026-04-12 17:20:31 +0300

    feat(api): name validator

--- api/src/data/validators.ts
+++ api/src/data/validators.ts
@@ -5,3 +5,10 @@ export const usernameSchema = z
   .min(3, "Username must be at least 3 characters")
   .max(20, "Username must be at most 20 characters")
   .regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores");
+
+export const nameSchema = z
+  .string()
+  .min(1, "Name must be at least 1 character")
+  .max(32, "name must be at most 32 characters")
+  .trim()
+  .regex(/^[\p{L}\p{N} '_\-.]+$/u);
