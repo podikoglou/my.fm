ff236ce3945c4efd1c86a1a8ee50ecd7758865e0
alex <alex.podikoglou@gmail.com>
2026-04-12 11:02:12 +0300

    feat(api): createJwt in jwt.ts

---
+++ api/src/jwt.ts
@@ -0,0 +1,16 @@
+import { sign } from "hono/jwt";
+import { env } from "./env";
+import type { User } from "./db/schema";
+
+const JWT_DURATION = Temporal.Duration.from({ hours: 24 });
+
+export type JwtPayload = {
+  id: User["id"];
+  exp: number;
+};
+
+export async function createJwt(userId: JwtPayload["id"]): Promise<string> {
+  const exp = Temporal.Now.instant().add(JWT_DURATION);
+
+  return sign({ userId, exp: exp.epochMilliseconds / 1000 }, env.SECRET);
+}
