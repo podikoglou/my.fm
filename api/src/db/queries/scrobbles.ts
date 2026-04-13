c817655b7a6ef9cf4aa3226a0d72aa8661e49f8b
alex <alex.podikoglou@gmail.com>
2026-04-13 16:29:55 +0300

    feat(api): addScrobble in queries/scrobbles.ts

---
+++ api/src/db/queries/scrobbles.ts
@@ -0,0 +1,14 @@
+import { nanoid } from "nanoid";
+import { db } from "..";
+import { scrobbles, type ScrobbleInsert } from "../schema";
+
+export async function addScrobble(
+  values: Pick<ScrobbleInsert, "userId" | "spotifyUri" | "scrobbleDate">,
+) {
+  const id = nanoid();
+
+  return await db
+    .insert(scrobbles)
+    .values({ ...values, id })
+    .returning();
+}
