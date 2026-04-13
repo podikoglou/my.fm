5a046de2133fb16a6dd01ce42cc668c21ce9f8e6
alex <alex.podikoglou@gmail.com>
2026-04-13 17:05:02 +0300

    refactor(api): addScrobble -> createScrobble

--- api/src/db/queries/scrobbles.ts
+++ api/src/db/queries/scrobbles.ts
@@ -2,7 +2,7 @@ import { nanoid } from "nanoid";
 import { db } from "..";
 import { scrobbles, type ScrobbleInsert } from "../schema";
 
-export async function addScrobble(
+export async function createScrobble(
   values: Pick<ScrobbleInsert, "userId" | "trackSpotifyUri" | "albumSpotifyUri" | "scrobbleDate">,
 ) {
   const id = nanoid();
