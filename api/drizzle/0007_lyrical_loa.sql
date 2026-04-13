e69de2acab1c0ed5aaeb0d6b7c7278f933a60751
alex <alex.podikoglou@gmail.com>
2026-04-13 16:45:35 +0300

    feat(api): albums table

---
+++ api/drizzle/0007_lyrical_loa.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `albums` (
+	`spotifyUri` text PRIMARY KEY NOT NULL,
+	`createdAt` integer DEFAULT (unixepoch()),
+	`name` text NOT NULL,
+	`releaseDate` text NOT NULL,
+	`totalTracks` integer NOT NULL,
+	`albumType` text NOT NULL,
+	`imageUrl` text NOT NULL
+);
--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -36,3 +36,22 @@ export const scrobbles = sqliteTable("scrobbles", {
 
 export type ScrobbleInsert = typeof scrobbles.$inferInsert;
 export type Scrobble = typeof scrobbles.$inferSelect;
+
+export const albums = sqliteTable("albums", {
+  spotifyUri: text().primaryKey(),
+  createdAt: integer({ mode: "timestamp" }).default(sql`(unixepoch())`),
+
+  name: text().notNull(),
+
+  // this is not always known to good precision and sometimes it may be a range
+  // rather than a concrete value, so we don't bother properly parsing it
+  releaseDate: text().notNull(),
+
+  totalTracks: integer().notNull(),
+
+  albumType: text().notNull(),
+  imageUrl: text().notNull(),
+});
+
+export type AlbumInsert = typeof albums.$inferInsert;
+export type Album = typeof albums.$inferSelect;
