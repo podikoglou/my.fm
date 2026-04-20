38bd2f2e934e083a8470d86a57231a1685a5d7fb
alex <alex.podikoglou@gmail.com>
2026-04-20 15:05:24 +0300

    fix(api): initialize username randomly

--- api/src/auth.ts
+++ api/src/auth.ts
@@ -47,7 +47,7 @@ export const auth = betterAuth({
           return {
             data: {
               ...user,
-              username: user.id,
+              username: nanoid(),
             },
           };
         },
