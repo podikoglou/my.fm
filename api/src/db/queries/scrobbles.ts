57a09d37db3686bfe38e2e0151b6d2eabefedf2c
alex <alex.podikoglou@gmail.com>
2026-04-20 17:08:41 +0300

    docs(api): document decision to use a hash instead of nanoid for scrobble ids

--- api/src/db/queries/scrobbles.ts
+++ api/src/db/queries/scrobbles.ts
@@ -5,6 +5,7 @@ import { scrobbles, type ScrobbleInsert, type User } from "../schema";
 export async function createScrobble(
   values: Pick<ScrobbleInsert, "userId" | "trackSpotifyUri" | "albumSpotifyUri" | "scrobbleDate">,
 ) {
+  // we use a hash here instead of a randomly generated ID to avoid re-adding the same scrobble if it happens to be fetched again (which probably won't happen)
   const id = Bun.hash.wyhash(`${values.userId}:${values.scrobbleDate}`).toString();
 
   return await db
